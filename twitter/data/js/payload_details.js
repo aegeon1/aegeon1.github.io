b9ea1f2da8582cd84949e81886a6ef47c72e1dcd
aegeon1 <46856970+aegeon1@users.noreply.github.com>
2019-02-27 15:17:06 -0500

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2,
-	"created_at": "Mon Feb 25 03:29:03 +0000 2019",
+	"tweets": 3,
+	"created_at": "Wed Feb 27 20:17:04 +0000 2019",
 	"lang": "en"
 }
--- twitter/data/js/tweets/2019_02.js
+++ twitter/data/js/tweets/2019_02.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Wed Feb 27 03:51:44 +0000 2019",
+		"id": 1100604369693741000,
+		"id_str": "1100604369693741057",
+		"full_text": "@insari88 Ya, me too!",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			21
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "insari88",
+					"name": "insari",
+					"id": 1099814767135547400,
+					"id_str": "1099814767135547392",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1100561751509254100,
+		"in_reply_to_status_id_str": "1100561751509254146",
+		"in_reply_to_user_id": 1099814767135547400,
+		"in_reply_to_user_id_str": "1099814767135547392",
+		"in_reply_to_screen_name": "insari88",
+		"user": {
+			"id": 1099813026922926100,
+			"id_str": "1099813026922926080",
+			"name": "ayrbumi",
+			"screen_name": "ayrbumi",
+			"location": "",
+			"description": "micro bloggin' for vacay and fun!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1,
+			"friends_count": 1,
+			"listed_count": 0,
+			"created_at": "Sun Feb 24 23:27:13 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1099813419820167169/Mhl7T8L7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099813419820167169/Mhl7T8L7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1099813026922926080/1551052426",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@insari88 Ya, me too!"
+	},
+	{
 		"created_at": "Mon Feb 25 01:32:58 +0000 2019",
 		"id": 1099844669889003500,
 		"id_str": "1099844669889003520",
