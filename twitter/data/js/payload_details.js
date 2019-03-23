266b5c1a227ed694eb0cfc8584b13c2d7718c7bd
aegeon1 <46856970+aegeon1@users.noreply.github.com>
2019-03-23 16:17:04 -0400

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4,
-	"created_at": "Sat Mar 09 20:17:03 +0000 2019",
+	"tweets": 5,
+	"created_at": "Sat Mar 23 20:17:03 +0000 2019",
 	"lang": "en"
 }
--- twitter/data/js/tweets/2019_03.js
+++ twitter/data/js/tweets/2019_03.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sat Mar 23 12:35:15 +0000 2019",
+		"id": 1109433423683301400,
+		"id_str": "1109433423683301376",
+		"full_text": "First time thru the tunnel to Billy Bishop Toronto #ytz https://t.co/E3jUzzbUCB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ytz",
+					"indices": [
+						51,
+						55
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1109433416628396000,
+					"id_str": "1109433416628396032",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/D2V_0HwVYAAa_ou.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2V_0HwVYAAa_ou.jpg",
+					"url": "https://t.co/E3jUzzbUCB",
+					"display_url": "pic.twitter.com/E3jUzzbUCB",
+					"expanded_url": "https://twitter.com/ayrbumi/status/1109433423683301376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1109433416628396000,
+					"id_str": "1109433416628396032",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/D2V_0HwVYAAa_ou.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2V_0HwVYAAa_ou.jpg",
+					"url": "https://t.co/E3jUzzbUCB",
+					"display_url": "pic.twitter.com/E3jUzzbUCB",
+					"expanded_url": "https://twitter.com/ayrbumi/status/1109433423683301376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1109433416628334600,
+					"id_str": "1109433416628334592",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/D2V_0HwUcAArMzy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2V_0HwUcAArMzy.jpg",
+					"url": "https://t.co/E3jUzzbUCB",
+					"display_url": "pic.twitter.com/E3jUzzbUCB",
+					"expanded_url": "https://twitter.com/ayrbumi/status/1109433423683301376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1109433416628355100,
+					"id_str": "1109433416628355073",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/D2V_0HwUwAE8NaN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2V_0HwUwAE8NaN.jpg",
+					"url": "https://t.co/E3jUzzbUCB",
+					"display_url": "pic.twitter.com/E3jUzzbUCB",
+					"expanded_url": "https://twitter.com/ayrbumi/status/1109433423683301376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1538,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1099813026922926100,
+			"id_str": "1099813026922926080",
+			"name": "ayrbumi",
+			"screen_name": "ayrbumi",
+			"location": "",
+			"description": "micro bloggin' for vacay and fun!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1,
+			"friends_count": 1,
+			"listed_count": 0,
+			"created_at": "Sun Feb 24 23:27:13 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1099813419820167169/Mhl7T8L7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099813419820167169/Mhl7T8L7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1099813026922926080/1551052426",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "07d9c93990c81000",
+			"url": "https://api.twitter.com/1.1/geo/id/07d9c93990c81000.json",
+			"place_type": "poi",
+			"name": "Billy Bishop Toronto City Airport (YTZ)",
+			"full_name": "Billy Bishop Toronto City Airport (YTZ)",
+			"country_code": "CA",
+			"country": "Canada",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-79.39615488052368,
+							43.631602429512775
+						],
+						[
+							-79.39615488052368,
+							43.631602429512775
+						],
+						[
+							-79.39615488052368,
+							43.631602429512775
+						],
+						[
+							-79.39615488052368,
+							43.631602429512775
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "First time thru the tunnel to Billy Bishop Toronto #ytz https://t.co/E3jUzzbUCB"
+	},
+	{
 		"created_at": "Sat Mar 09 19:33:04 +0000 2019",
 		"id": 1104465141490479100,
 		"id_str": "1104465141490479104",
