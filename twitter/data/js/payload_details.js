952067bb70832adc21d4408295ebfb3e24d5babd
aegeon1 <46856970+aegeon1@users.noreply.github.com>
2019-03-25 16:17:06 -0400

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5,
-	"created_at": "Sat Mar 23 20:17:03 +0000 2019",
+	"tweets": 6,
+	"created_at": "Mon Mar 25 20:17:05 +0000 2019",
 	"lang": "en"
 }
--- twitter/data/js/tweets/2019_03.js
+++ twitter/data/js/tweets/2019_03.js
@@ -1,6 +1,178 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sun Mar 24 22:58:02 +0000 2019",
+		"id": 1109952542648094700,
+		"id_str": "1109952542648094720",
+		"full_text": "First breakfast @insari88 in Tokyo https://t.co/HHQSO7KlTp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "insari88",
+					"name": "insari",
+					"id": 1099814767135547400,
+					"id_str": "1099814767135547392",
+					"indices": [
+						16,
+						25
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1109952536457277400,
+					"id_str": "1109952536457277440",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/D2dX830UcAAD1Ep.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2dX830UcAAD1Ep.jpg",
+					"url": "https://t.co/HHQSO7KlTp",
+					"display_url": "pic.twitter.com/HHQSO7KlTp",
+					"expanded_url": "https://twitter.com/ayrbumi/status/1109952542648094720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1109952536457277400,
+					"id_str": "1109952536457277440",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/D2dX830UcAAD1Ep.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2dX830UcAAD1Ep.jpg",
+					"url": "https://t.co/HHQSO7KlTp",
+					"display_url": "pic.twitter.com/HHQSO7KlTp",
+					"expanded_url": "https://twitter.com/ayrbumi/status/1109952542648094720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1099813026922926100,
+			"id_str": "1099813026922926080",
+			"name": "ayrbumi",
+			"screen_name": "ayrbumi",
+			"location": "",
+			"description": "micro bloggin' for vacay and fun!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1,
+			"friends_count": 1,
+			"listed_count": 0,
+			"created_at": "Sun Feb 24 23:27:13 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1099813419820167169/Mhl7T8L7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099813419820167169/Mhl7T8L7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1099813026922926080/1551052426",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "First breakfast @insari88 in Tokyo https://t.co/HHQSO7KlTp"
+	},
+	{
 		"created_at": "Sat Mar 23 12:35:15 +0000 2019",
 		"id": 1109433423683301400,
 		"id_str": "1109433423683301376",
