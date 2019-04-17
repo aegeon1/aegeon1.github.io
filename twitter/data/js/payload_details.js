80300dce950acec61b25bbdc92a4d1245b525c3a
aegeon1 <46856970+aegeon1@users.noreply.github.com>
2019-04-17 16:17:06 -0400

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8,
-	"created_at": "Sat Mar 30 20:17:04 +0000 2019",
+	"tweets": 9,
+	"created_at": "Wed Apr 17 20:17:04 +0000 2019",
 	"lang": "en"
 }
---
+++ twitter/data/js/tweets/2019_04.js
@@ -0,0 +1,175 @@
+Grailbird.data.tweets_2019_04 = 
+[
+	{
+		"created_at": "Tue Apr 16 23:02:08 +0000 2019",
+		"id": 1118288494298849300,
+		"id_str": "1118288494298849281",
+		"full_text": "Winning our Rilakuma in Shinjuku with @insari88 https://t.co/ovrNLz3NzD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "insari88",
+					"name": "insari",
+					"id": 1099814767135547400,
+					"id_str": "1099814767135547392",
+					"indices": [
+						38,
+						47
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1118288486568611800,
+					"id_str": "1118288486568611840",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/media/D4T1dB5U0AAPZMh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4T1dB5U0AAPZMh.jpg",
+					"url": "https://t.co/ovrNLz3NzD",
+					"display_url": "pic.twitter.com/ovrNLz3NzD",
+					"expanded_url": "https://twitter.com/ayrbumi/status/1118288494298849281/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 901,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1538,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1118288486568611800,
+					"id_str": "1118288486568611840",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/media/D4T1dB5U0AAPZMh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4T1dB5U0AAPZMh.jpg",
+					"url": "https://t.co/ovrNLz3NzD",
+					"display_url": "pic.twitter.com/ovrNLz3NzD",
+					"expanded_url": "https://twitter.com/ayrbumi/status/1118288494298849281/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 901,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1538,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1099813026922926100,
+			"id_str": "1099813026922926080",
+			"name": "ayrbumi",
+			"screen_name": "ayrbumi",
+			"location": "",
+			"description": "micro bloggin' for vacay and fun!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1,
+			"friends_count": 1,
+			"listed_count": 0,
+			"created_at": "Sun Feb 24 23:27:13 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1099813419820167169/Mhl7T8L7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099813419820167169/Mhl7T8L7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1099813026922926080/1551052426",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "Winning our Rilakuma in Shinjuku with @insari88 https://t.co/ovrNLz3NzD"
+	}
+]
