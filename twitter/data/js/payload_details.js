db91cba1a3f008302c1675c5976b5968b90b14e8
aegeon1 <46856970+aegeon1@users.noreply.github.com>
2020-03-15 16:17:04 -0400

    Added 1 tweets

--- twitter/data/js/payload_details.js
+++ twitter/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9,
-	"created_at": "Wed Apr 17 20:17:04 +0000 2019",
+	"tweets": 10,
+	"created_at": "Sun Mar 15 20:17:03 +0000 2020",
 	"lang": "en"
 }
--- twitter/data/js/tweet_index.js
+++ twitter/data/js/tweet_index.js
@@ -1,5 +1,12 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2020_03.js",
+		"year": 2020,
+		"var_name": "tweets_2020_03",
+		"tweet_count": 1,
+		"month": 3
+	},
+	{
 		"file_name": "data/js/tweets/2019_04.js",
 		"year": 2019,
 		"var_name": "tweets_2019_04",
